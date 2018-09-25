afb8e9aed7276d98dd6b889af67d3df277c1ef37
Vicenc Garcia-Altes <vicentredrum@gmail.com>
2018-09-25 11:11:50 +0100

    Tweaking

--- functions/service-a.js
+++ functions/service-a.js
@@ -1,26 +1,28 @@
 'use strict';
 
-const _        = require('lodash');
-const Promise  = require('bluebird');
-const utils    = require('./utils');
-const AWS      = require('aws-sdk');
-const sns      = Promise.promisifyAll(new AWS.SNS());
-const s3       = Promise.promisifyAll(new AWS.S3());
+const _ = require('lodash');
+const Promise = require('bluebird');
+const utils = require('./utils');
+const AWS = require('aws-sdk');
+const sns = Promise.promisifyAll(new AWS.SNS());
+const s3 = Promise.promisifyAll(new AWS.S3());
 const dynamodb = Promise.promisifyAll(new AWS.DynamoDB.DocumentClient());
-const AWSXRay  = require('aws-xray-sdk');
-const lambda   = new AWS.Lambda();
-const region   = AWS.config.region;
+const AWSXRay = require('aws-xray-sdk');
+const lambda = new AWS.Lambda();
+const region = AWS.config.region;
 const BUCKET_NAME = process.env.BUCKET_NAME;
+
+console.log(`IOPIPE TOKEN: ${process.env.iopipe_token}`);
 
 const iopipe = require('@iopipe/iopipe')({
   token: `${process.env.iopipe_token}`
 });
 
-let publishSNS = function(segment){
+let publishSNS = function (segment) {
   return new Promise((resolve, reject) => {
     console.log("publishing to sns");
 
-    let doPublish = async function(subsegment){
+    let doPublish = async function (subsegment) {
       let topicArn = `arn:aws:sns:${region}:${global.accountId}:serverless-observability-${process.env.stage}`;
       let message = 'test';
 
@@ -30,7 +32,7 @@ let publishSNS = function(segment){
       };
 
       subsegment.addAnnotation('topic', topicArn);
-      subsegment.addMetadata('message', message);  
+      subsegment.addMetadata('message', message);
 
       await sns.publishAsync(req);
       subsegment.close();
@@ -46,7 +48,7 @@ let invokeLambda = segment => {
   return new Promise((resolve, reject) => {
     console.log('invoking Lambda function');
 
-    let doInvoke = async function(subsegment){
+    let doInvoke = async function (subsegment) {
       let funcName = `${process.env.service}-${process.env.stage}-service-c`;
 
       subsegment.addAnnotation('function', funcName);
@@ -75,39 +77,39 @@ let accessDynamoDB = (segment) => {
   return new Promise((resolve, reject) => {
     console.log("accessing dynamo db");
 
-    let doAccess = async function(subsegment){
-        let table = `serverless-observability-${process.env.stage}`;
-        let id = global.requestId;
-        let value = `test-${id}`;
-        let getReq = {
-          TableName: table,
-          Key: {
-            id: value
-          }
-        };
+    let doAccess = async function (subsegment) {
+      let table = `serverless-observability-${process.env.stage}`;
+      let id = global.requestId;
+      let value = `test-${id}`;
+      let getReq = {
+        TableName: table,
+        Key: {
+          id: value
+        }
+      };
 
-        let putReq = {
-          TableName: table,
-          Item: {
-            id: value,
-          }
-        };
+      let putReq = {
+        TableName: table,
+        Item: {
+          id: value,
+        }
+      };
 
-        subsegment.addAnnotation('table', table);
-        subsegment.addAnnotation('id', id);
-        subsegment.addMetadata('value', value);
+      subsegment.addAnnotation('table', table);
+      subsegment.addAnnotation('id', id);
+      subsegment.addMetadata('value', value);
 
-        await Promise.all([
-          dynamodb.getAsync(getReq),
-          dynamodb.putAsync(putReq)
-        ]);
+      await Promise.all([
+        dynamodb.getAsync(getReq),
+        dynamodb.putAsync(putReq)
+      ]);
 
-        subsegment.close();
+      subsegment.close();
 
-        resolve();
-      }
-    
-      AWSXRay.captureAsyncFunc("## accessing DynamoDB", doAccess, segment);
+      resolve();
+    }
+
+    AWSXRay.captureAsyncFunc("## accessing DynamoDB", doAccess, segment);
   });
 };
 
@@ -115,7 +117,7 @@ let accessS3 = (segment) => {
   return new Promise((resolve, reject) => {
     console.log("accessing s3");
 
-    let doAccess = async function(subsegment){
+    let doAccess = async function (subsegment) {
       let bucket = BUCKET_NAME;
       let key = `${global.requestId}.txt`;
       let body = 'test';
@@ -129,7 +131,7 @@ let accessS3 = (segment) => {
         Bucket: bucket,
         Key: key
       };
-      
+
       await s3.putObjectAsync(putReq);
 
       subsegment.close();
@@ -145,23 +147,23 @@ let callServiceB = (n, segment) => {
   return new Promise((resolve, reject) => {
     console.log("calling service b");
 
-    let doCall = async function(subsegment){
+    let doCall = async function (subsegment) {
       subsegment.addAnnotation('path', '/dev/demo/service-b');
 
-        let resp = await utils.request('GET', global.hostname, '/dev/demo/service-b');
+      let resp = await utils.request('GET', global.hostname, '/dev/demo/service-b');
 
-        console.log(resp);
-        let body = JSON.parse(resp);
+      console.log(resp);
+      let body = JSON.parse(resp);
 
-        subsegment.addMetadata('message', body.message);
+      subsegment.addMetadata('message', body.message);
 
-        subsegment.close();
+      subsegment.close();
 
-        resolve(body.message);
-      };
+      resolve(body.message);
+    };
 
-      AWSXRay.captureAsyncFunc("## calling service-b via http", doCall, segment);
-    });
+    AWSXRay.captureAsyncFunc("## calling service-b via http", doCall, segment);
+  });
 };
 
 module.exports.handler = iopipe(async function (event, context, callback) {
@@ -194,11 +196,37 @@ module.exports.handler = iopipe(async function (event, context, callback) {
     console.log("service-a is going to call the timeout endpoint");
     await utils.request('GET', hostname, '/dev/demo/timeout');
 
-    throw new Error("timed out");
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `Timed out`
+      })
+    };
+
+    callback(null, response);
+  } else if (n <= 3) {
+    console.log("service-a is going to call the error endpoint");
+    await utils.request('GET', hostname, '/dev/demo/error');
+
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: `Custom internal server error`
+      })
+    };
+
+    callback(null, response);
   } else {
     console.log("service-a is going to call the error endpoint");
     await utils.request('GET', hostname, '/dev/demo/error');
 
-    throw new Error("boom");
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `This shouldn't appear`
+      }),
+    };
+
+    callback(null, response);
   }
 });
